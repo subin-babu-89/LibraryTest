41a9513947e2a1cc990d57777402a22496540794
Subin Babu <subin@telmate.com>
2019-04-17 21:04:30 -0700

    Functions without classes

--- libtest/src/main/java/net/gtl/libtest/FunctionWithoutClass.kt
+++ libtest/src/main/java/net/gtl/libtest/FunctionWithoutClass.kt
@@ -1,3 +1,7 @@
 package net.gtl.libtest
 
+val libregClassTest = LibRegClassTest()
+
 fun justAnotherFunction() = "Called without a class"
+
+fun callMethodFromClass() = libregClassTest.regFuncCall()
