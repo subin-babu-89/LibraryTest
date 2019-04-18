1d45a11c850f058fc273a274cdf6b4496bc512af
Subin Babu <subin@telmate.com>
2019-04-17 20:42:55 -0700

    Regular Class added

---
+++ libtest/src/main/java/net/gtl/libtest/LibRegClassTest.kt
@@ -0,0 +1,8 @@
+package net.gtl.libtest
+
+class LibRegClassTest {
+
+    fun regFuncCall() = "Reg Function called"
+    private fun privFuncCall() = "Priv Function called"
+    protected fun proFuncCall() = "Protected Function called"
+}
